2ae6e6ea7d9754d1dc6effe879b155a4633e5f7f
GuyTurevich <guy938@gmail.com>
2022-01-12 02:56:53 +0200

    changed structure

--- Repository.py
+++ Repository.py
@@ -1,18 +1,14 @@
 import atexit
 import sqlite3
+from DAO import Suppliers, Hats, Orders
+import sys
 
-from Hats import Hats
-from Orders import Orders
-from Suppliers import Suppliers
-import os
+from DTO import Supplier, Hat, Order
 
 
 class Repository:
-
     def __init__(self):
-        if os.path.exists('database.db'):
-            os.remove('database.db')
-        self.connection = sqlite3.connect('database.db')
+        self.connection = sqlite3.connect(sys.argv[4])
         self.hats = Hats(self.connection)
         self.suppliers = Suppliers(self.connection)
         self.orders = Orders(self.connection)
@@ -43,6 +39,33 @@ class Repository:
         FOREIGN KEY(hat)    REFERENCES hats(id)
         );
         """)
+
+    def read_config(self, config):
+        first_line = config[0].split(',')
+        config_num_of_entries = int(first_line[0])
+
+        for i in range(config_num_of_entries + 1, len(config)):
+            entry = config[i].rstrip("\n").split(',')
+            supplier = Supplier(entry[0], entry[1])
+            self.suppliers.insert(supplier)
+
+        for i in range(1, config_num_of_entries + 1):
+            entry = config[i].rstrip("\n").split(',')
+            hat = Hat(entry[0], entry[1], entry[2], entry[3])
+            self.hats.insert(hat)
+
+    def execute_orders(self, orders):
+        summary = ""
+        for i in range(len(orders)):
+            entry = orders[i].rstrip("\n").split(',')
+            location = entry[0]
+            topping = entry[1]
+            order = Order(i + 1, location, self.hats.find(topping))
+            self.orders.insert(order)
+            supplier = self.suppliers.find(self.hats.get_supplier_id(order.hat))
+            summary += topping + "," + supplier[0] + "," + location + "\n"
+            self.hats.check_quantity(order.hat)
+        return summary
 
 
 repo = Repository()
--- main.py
+++ main.py
@@ -1,41 +1,16 @@
-from Hats import Hat, Hats
-from Orders import Order, Orders
 from Repository import repo
-from Suppliers import Supplier, Suppliers
+import sys
 
-with open('config.txt', 'r') as file:
-    config = file.readlines()
-firstLine = config[0].split(',')
-configNumOfEntries = int(firstLine[0])
-configNumOfSuppliers = int(firstLine[1])
+
 repo.create_tables()
-Hats(repo.connection)
-Suppliers(repo.connection)
-Orders(repo.connection)
-summary = ""
 
-for i in range(configNumOfEntries + 1, len(config)):
-    entry = config[i].rstrip("\n").split(',')
-    supplier = Supplier(entry[0], entry[1])
-    repo.suppliers.insert(supplier)
+with open(sys.argv[1], 'r') as config_file:
+    config = config_file.readlines()
+repo.read_config(config)
 
-for i in range(1, configNumOfEntries + 1):
-    entry = config[i].rstrip("\n").split(',')
-    hat = Hat(entry[0], entry[1], entry[2], entry[3])
-    repo.hats.insert(hat)
+with open(sys.argv[2], 'r') as orders_file:
+    orders = orders_file.readlines()
+summary = repo.execute_orders(orders)
 
-with open('orders.txt', 'r') as file:
-    orders = file.readlines()
-
-for i in range(len(orders)):
-    entry = orders[i].rstrip("\n").split(',')
-    location = entry[0]
-    topping = entry[1]
-    order = Order(i+1, location, repo.hats.get_hat_id(topping))
-    repo.orders.insert(order)
-    supplier = repo.suppliers.get_supplier(repo.hats.get_supplier_id(order.hat))
-    summary += topping + "," + supplier + "," + location + "\n"
-    repo.hats.check_quantity(order.hat)
-
-with open('summary.txt', 'w') as file:
-    file.write(summary)
+with open(sys.argv[3], 'w') as output_file:
+    output_file.write(summary)
